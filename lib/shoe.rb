c88b4e44236277076775de97d61ff2327940e55f
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-30 01:18:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,9 @@
 # Make your shoe class here!
 
 class Shoe
-  
-  attr_reader :brand
-  
-  def initalize(brand)
-    @brand = brand 
-  end 
-  
+ 
+ def initialize(brand)
+   @brand = brand 
+ end 
+ 
 end 
